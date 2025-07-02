0df56fbb02328fb0cb9d1847bc07a6018ff1cbf1
2024.4.14 <2024.4.14@DESKTOP-I4U5BN5>
2025-07-01 22:57:33 -0300

    Bate papo Server

--- BatePapoClient/Form1.cs
+++ BatePapoClient/Form1.cs
@@ -5,20 +5,40 @@ namespace BatePapoClient
 {
     public partial class Form1 : Form
     {
-        private HubConnection connection = null;
+        private HubConnection _connection = null;
         public Form1()
         {
             InitializeComponent();
             InitializeSignalR();
         }
 
-        private void InitializeSignalR()
+        private async void InitializeSignalR()
         {
-            throw new NotImplementedException();
-        }
+            _connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/chathub")
+                .Build();
+            _connection.On<string, string>("ReceiveMessage", (user, message)) =>
+                {
+                this.Invoke((Action)(() =>
+                    {
+                        lstMessages.Items.Add($"{user}: {message}");
+                    }));
+            });
+            try
+            {
+                await _connection.StartAsync();
+            }
+            catch (Exception ex)
+
+            {
+                lstMessages.Items.Add($"Erro ao conectar: {ex.Message}");
+            }
+        }               
+        
 
         private void btnEnviar_Click(object sender, EventArgs e)
         {
+
 
         }
     }
--- BatePapoServer/Program.cs
+++ BatePapoServer/Program.cs
@@ -1,6 +1,64 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+using BatePapoServer.Hubs;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//configuração MongoDB
+var connectionString = builder.Configuration.GetConnectionString("MongoDb");
+var client = new MongoClient(connectionString);
+var database = client.GetDatabase("ChatHub");
+builder.Services.AddSingleton(database);
+try
+{
+    var collections = database.ListCollectionNames().ToList();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro ao conectar ao MongoDB: {ex.ToString()}");
+}
+//Inserir documento inicial na coleção TestCollerction
+var collection = database.GetCollection<BsonDocument>("TestCollection");
+if(collection.CountDocuments(new BsonDocument()) == 0)
+{
+    collection.InsertOne(new BsonDocument("init",true));
+}
+//Testando conexão com o MongoDb e listando banco de dados
+var testClient = new MongoClient("mongodb://localhost:27017");
+var dbs = testClient.ListDatabaseNames().ToList();
+Console.WriteLine(string.Join(",", dbs));
+//Configura SignalR
+builder.Services.AddSignalR();
+//Add services to the container
+builder.Services.AddControllers();
+//Configuração permissões CORS mais ampla para o desenvolvimento
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        {
+            policy.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .SetIsOriginAllowed(host => true);//permitir qualquer origem acessar
+        });
+
+});
+builder.WebHost.UseUrls("https://localhost:5000");
+var app = builder.Build();
+//Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors();
+}
+app.UseRouting();
+app.UseAuthorization();
+app.MapHub<ChatHub>("/chathub");
+app.MapControllers();
+app.MapGet("/", () => "API SignalR Chat Server rodondo");
+app.Run();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
